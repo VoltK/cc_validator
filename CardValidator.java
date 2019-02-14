8fcb0ccc55ff2853e81e834ce24ac22fdd73b6dc
VoltK <42512548+VoltK@users.noreply.github.com>
2019-02-14 15:11:21 -0600

    Erased unnecessary loop for convertion

--- CardValidator.java
+++ CardValidator.java
@@ -8,10 +8,7 @@ public class CardValidator {
         String card = JOptionPane.showInputDialog("Enter card number to validate: ");
 
         if(card.matches("[0-9]+")){
-
-            int [] cardArr = getNumbers(card);
-
-            int sum = calculateSum(cardArr);
+            int sum = calculateSum(card);
 
             showResult(sum, card);
         }
@@ -21,24 +18,13 @@ public class CardValidator {
         }
 
     }
-
-    // Convert string to Int array
-    public static int [] getNumbers(String number){
-        int [] numbers_arr = new int[number.length()];
-
-        for (int x = 0; x < numbers_arr.length; x++){
-            numbers_arr[x] = Character.getNumericValue(number.charAt(x));
-        }
-
-        return numbers_arr;
-    }
-
-    public static int calculateSum(int [] numbers){
+    
+    public static int calculateSum(String numbers){
         int sum = 0;
 
-        for (int x = numbers.length - 2; x >= 0; x-=2){
-            int evenNumber = numbers[x] * 2;
-            int oddNumber = numbers[x+1];
+        for (int x = numbers.length() - 2; x >= 0; x-=2){
+            int evenNumber = Character.getNumericValue(numbers.charAt(x)) * 2;
+            int oddNumber = Character.getNumericValue(numbers.charAt(x+1));
 
             // if doubled number is 2 digits number we add those 2 digits together( or just subtract 9)
             sum += evenNumber > 9 ? evenNumber - 9 + oddNumber: evenNumber + oddNumber;
